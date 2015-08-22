0ccf4fb997019de3c829d0c935ac4cb51df21d65
Matthew Isom <isom.matt@gmail.com>
2015-08-22 00:51:51 -0500

    Standard Assets
    Added in a texture too

---
+++ Wolvesbane/Assets/Scripts/AboveView2DUserControl.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class AboveView2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump;
+
+
+        private void Awake()
+        {
+            m_Character = GetComponent<PlatformerCharacter2D>();
+        }
+
+
+        private void Update()
+        {
+            if (!m_Jump)
+            {
+                // Read the jump input in Update so button presses aren't missed.
+                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+            float move_x = CrossPlatformInputManager.GetAxis("Horizontal");
+			float move_y = CrossPlatformInputManager.GetAxis("Vertical");
+            // Pass all parameters to the character control script.
+            m_Character.Move(move_x,move_y);
+            m_Jump = false;
+        }
+    }
+}
---
+++ Wolvesbane/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+    public class PlatformerCharacter2D : MonoBehaviour
+    {
+        [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x & y axis.
+		
+        private Animator m_Anim;            // Reference to the player's animator component.
+        private Rigidbody2D m_Rigidbody2D;
+        private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+
+        private void Awake()
+        {
+            m_Anim = GetComponent<Animator>();
+            m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            
+        }
+
+
+		public void Move(float move_x, float move_y)
+        {
+
+            // The Speed animator parameter is set to the absolute value of the horizontal input.
+            //m_Anim.SetFloat("Speed", Mathf.Abs(move));
+
+            // Move the character
+			m_Rigidbody2D.velocity = new Vector2(move_x*m_MaxSpeed, move_y*m_MaxSpeed);
+
+            // If the input is moving the player right and the player is facing left...
+            /*if (move > 0 && !m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+                // Otherwise if the input is moving the player left and the player is facing right...
+            else if (move < 0 && m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }*/
+        }
+
+
+        private void Flip()
+        {
+            // Switch the way the player is labelled as facing.
+            m_FacingRight = !m_FacingRight;
+
+            // Multiply the player's x local scale by -1.
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
