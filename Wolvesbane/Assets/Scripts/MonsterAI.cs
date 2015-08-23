0ad0b9360576fc242f2a5a071bd947c110f34c7d
James Shepherd <iamjamesshepherd@gmail.com>
2015-08-23 16:06:59 -0500

    Mosters Animate
    Hopefully correct - check spawned werewolves?

--- Wolvesbane/Assets/Scripts/MonsterAI.cs
+++ Wolvesbane/Assets/Scripts/MonsterAI.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class MonsterAI : CreatureBase
 {
+	private Animator anim;
+
 	public float moveSpeed;
 	public int rotationSpeed;
 	
 	void Start() 
 	{
+		anim = GetComponent<Animator> ();
 		maxHealth = health;
 		/*health = 10;
 		armor = 1;
@@ -20,8 +23,42 @@ public class MonsterAI : CreatureBase
 	}
 	
 	void Update() 
-	{    
+	{   
 		if (target != null) {
+			// Read the inputs.
+			Vector3 direction = target.transform.position - transform.position;
+			float move_x = direction.x;
+			float move_y = direction.y;
+
+			//Animation logic
+			if (Mathf.Abs(move_x) > Mathf.Abs(move_y)) {
+				anim.SetBool ("Walking", true);
+				if (move_x > 0) {
+					anim.SetFloat("MoveX", 1f);
+					anim.SetFloat("LastMoveX", 1f);
+				} else {
+					anim.SetFloat("MoveX", -1f);
+					anim.SetFloat("LastMoveX", -1f);
+				}
+				anim.SetFloat("MoveY", 0f);
+				anim.SetFloat("LastMoveY", 0f);
+			} else if (move_y != 0) {
+				anim.SetBool ("Walking", true);
+				if (move_y > 0) {
+					anim.SetFloat("MoveY", 1f);
+					anim.SetFloat("LastMoveY", 1f);
+				} else {
+					anim.SetFloat("MoveY", -1f);
+					anim.SetFloat("LastMoveY", -1f);
+				}
+				anim.SetFloat("MoveX", 0f);
+				anim.SetFloat("LastMoveX", 0f);
+			} else {
+				anim.SetFloat("MoveX", anim.GetFloat("LastMoveX"));
+				anim.SetFloat("MoveY", anim.GetFloat("LastMoveY"));
+				anim.SetBool ("Walking", false);
+			}
+
 			if (stunTimer == 0) {
 				Vector3 dir = target.transform.position - transform.position;
 				// Only needed if objects don't share 'z' value.
@@ -44,6 +81,7 @@ public class MonsterAI : CreatureBase
 		} else {
 			target = GameObject.FindGameObjectWithTag("Player");
 		}
+
 		GetComponent<Rigidbody2D>().velocity = Vector3.zero;
 		if (hitTimer > 0) {
 			--hitTimer;
