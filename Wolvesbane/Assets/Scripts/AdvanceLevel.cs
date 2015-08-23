26c2488b1e7a68bdcd8e362892746932ed9e91ee
Mills McArthur <millsmcarthur@Millss-MacBook-Air.local>
2015-08-23 11:53:32 -0400

    Level Advancer Squares
    Creates a trigger point prefab for scene transitions.

---
+++ Wolvesbane/Assets/Scripts/AdvanceLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdvanceLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnTriggerEnter2D (Collider2D other) {
+		print("Exit point");
+		Application.LoadLevel("main");
+	}
+}
---
+++ Wolvesbane/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+	// Note: currently using the AdvanceLevel script instead of this function to do scene transitions.
+	private void OnTriggerEnter2D (Collider2D other) { 
+		//Check if the tag of the trigger collided with is Exit.
+		if (other.tag == "Exit") {
+			print ("Exit point");
+		}
+	}
+}
