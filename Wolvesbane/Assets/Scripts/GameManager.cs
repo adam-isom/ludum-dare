5214d043f258e9cd7120c43d9433c3079389fc33
Mills McArthur <millsmcarthur@Millss-MacBook-Air.local>
2015-08-22 20:27:48 -0400

    GameManager Script
    Added GameManager script, which is attached to the GameManager game
    object.

---
+++ Wolvesbane/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Start() function called.");
+		//Application.LoadLevel("main");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//This is called each time a scene is loaded.
+	void OnLevelWasLoaded(int level) {
+		print ("New level loaded.");
+	}
+}
