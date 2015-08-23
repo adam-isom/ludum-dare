a8f070762dffe3267d9c5684ff29bb1527d83c0e
Mills McArthur <millsmcarthur@Millss-MacBook-Air.local>
2015-08-23 12:45:24 -0400

    Level Text Overlay
    Displays a text overlay at the start of the scene.

--- Wolvesbane/Assets/Scripts/GameManager.cs
+++ Wolvesbane/Assets/Scripts/GameManager.cs
@@ -1,7 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+	private int level;
+	private bool doingSetup = true;
+	private Text levelText;									//Text to display current level number.
+	private GameObject levelImage;							//Image to block out level as levels are being set up, background for levelText.
+
+	//Awake is always called before any Start functions
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+
+		//Call the InitGame function to initialize the level 
+		InitGame();
+	}
+
+
+	//Initializes the game for each level.
+	void InitGame() {
+		//While doingSetup is true the player can't move, prevent player from moving while title card is up.
+		doingSetup = true;
+
+		//Get a reference to our image LevelImage by finding it by name.
+		levelImage = GameObject.Find("LevelImage");
+		//Get a reference to our text LevelText's text component by finding it by name and calling GetComponent.
+		levelText = GameObject.Find("LevelText").GetComponent<Text>();
+
+		//Set the text of levelText to the current level number.
+		level = Application.loadedLevel;
+		levelText.text = "Level " + level + "\n Press space to continue";
+		
+		//Set levelImage to active blocking player's view of the game board during setup.
+		levelImage.SetActive(true);
+	}
+
 
 	// Use this for initialization
 	void Start () {
@@ -11,9 +45,14 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown ("space") && Application.loadedLevel != 1) { // Application.loadedLevel is the current scene number
+
+		//Hides black image used between levels
+		if (Input.GetKeyDown ("space") && doingSetup == true) {
 			print ("space key was pressed");
-			Application.LoadLevel("main");
+
+			levelImage.SetActive(false);
+			levelText.enabled = false;			
+			doingSetup = false;
 		}
 	}
 
